927bc3fc933b999b41040c2683647d5b63803379
unix_system <4990534+unix-system@users.noreply.github.com>
2021-10-22 18:36:34 +0100

    Fix meta tag for embeds

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
+      description="Public documentation for Daxrentha's SCPF <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
